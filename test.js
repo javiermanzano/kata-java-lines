be4556dca89ae79293cbcfae42f90ae7ed2a9233
feliun <felipe.polo@gmail.com>
2018-10-26 16:41:18 +0200

    Refactor: common values to variables

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ const expect = require('expect.js');
 let processor;
 
 describe('Java line counter', () => {
+	const validLine = 'public interface Dave {}';
+	const lineComment = '// This is a comment';
+
 	beforeEach(() => {
 		processor = initProcessor();
 	});
@@ -14,25 +17,24 @@ describe('Java line counter', () => {
 	});
 
 	it('digests a single line', () => {
-		processor.digest('public interface Dave {');
+		processor.digest(validLine);
 		expect(processor.state).to.eql({
 			count: 1,
 		});
 	});
 
 	it('digests two lines', () => {
-		processor.digest('public interface Dave {');
-		processor.digest('  private String getName() {');
+		processor.digest(validLine);
+		processor.digest(validLine);
 		expect(processor.state).to.eql({
 			count: 2,
 		});
 	});
 
 	it('digests zero lines with one line comment', () => {
-		processor.digest('// this is a comment');
+		processor.digest(lineComment);
 		expect(processor.state).to.eql({
 			count: 0,
 		});
 	});
-
 });
