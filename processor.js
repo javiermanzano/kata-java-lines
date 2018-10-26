ab831e5967515731fa9d840b0f51dc5a5f46fcf9
feliun <felipe.polo@gmail.com>
2018-10-26 16:21:27 +0200

    Refactor to simple API

--- processor.js
+++ processor.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-	const start = () => ({});
+	const state = { count: 0 };
 
-	return { start };
+	return { state };
 };
--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-const { start } = require('./processor')();
+const initProcessor = require('./processor');
 const expect = require('expect.js');
+let processor;
 
 describe('Java line counter', () => {
-	it('returns an empty object when we start', () => {
-		const response = start();
-		expect(response).to.eql({});
+	before(() => {
+		processor = initProcessor();
+	});
+
+	it('returns the current state when a new processor is initialised', () => {
+		expect(processor.state).to.eql({
+			count: 0,
+		});
 	});
 });
